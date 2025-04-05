da1feef39b4e70437d0e0b50d66eaa45547dd436
detsabirde <130311972+detsabirde@users.noreply.github.com>
2025-04-05 02:02:47 +0100

    - final touch

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.use('/', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);    
 });
